a4a6cd9e84333c33da7ee9785784dd554a989561
Arahael <maciej.kornecki@gmail.com>
2023-04-19 16:36:26 +0200

    Fixed math in main.py; Minor changes in charts

--- main.py
+++ main.py
@@ -3,61 +3,57 @@ import numpy as np
 import pandas as pd
 
 #Input variables
-pressure = input("Set pressure: ")
-temperature = input("Set temperature: ")
-distance = 400
-ownElevation = input("Set your elevation: ")
-targetElevation = input("Set targets elevation: ")
-sideWind = input("Set side wind speed: ")
-wind = input("Set fron/back wind (back wind is negative value): ")
+pressure = float(input("Set pressure: "))
+temperature = float(input("Set temperature: "))
+distance = float(input("Set distance to the target: "))
+ownElevation = float(input("Set your elevation: "))
+targetElevation = float(input("Set targets elevation: "))
+sideWind = float(input("Set side wind speed: "))
+wind = float(input("Set front/back wind (back wind is negative value): "))
 
 #Placeholder space for switch to choose proper csv
 
 #Temporary csv import
 ballisticTable = pd.read_csv('BallisticData/Mk6_charge1.csv')
 
-print(ballisticTable.columns)
-print(ballisticTable.columns.size)
-closestDistL = ballisticTable.loc[ballisticTable['Dystans'] <= distance].tail(1)
+#print(ballisticTable.columns)
+#print(ballisticTable.columns.size)
+closestDistRowL = ballisticTable.loc[ballisticTable['Dystans'] <= distance].tail(1)
 
-closestDistH = ballisticTable.loc[ballisticTable['Dystans'] > distance].head(1)
+closestDistRowH = ballisticTable.loc[ballisticTable['Dystans'] > distance].head(1)
 
 #Read of the csv
-print("Closest smaller row:")
-print(closestDistL)
-print("\nClosest greater row:")
-print(closestDistH)
-#closestDistL=1
-#closestDistH=1
-closestElevL=1
-closestElevH=1
-closestDElev100L=1
-closestDElev100H=1
-dFlightTimeL=1
-dFlightTimeH=1
-flightTimeL=1
-flightTimeH=1
-windageL=1
-windageH=1
-frontWindL=1
-frontWindH=1
-backWindL=1
-backWindH=1
-tempDecL=1
-tempDecH=1
-tempIncL=1
-tempIncH=1
-airDensDecL=1
-airDensDecH=1
-airDensIncL=1
-airDensIncH=1
+closestElevL = closestDistRowL['Elewacja'].values
+closestElevH = closestDistRowH['Elewacja'].values
+closestDElev100L = closestDistRowL['D elew/100m'].values
+closestDElev100H = closestDistRowH['D elew/100m'].values
+dFlightTimeL = closestDistRowL['D CzL/100'].values
+dFlightTimeH = closestDistRowH['D CzL/100'].values
+flightTimeL = closestDistRowL['CzL'].values
+flightTimeH = closestDistRowH['CzL'].values
+windageL = closestDistRowL['Azymut Xwiatr 1m/s'].values
+windageH = closestDistRowH['Azymut Xwiatr 1m/s'].values
+
+#Check if wind is frontal or from the back?
+frontWindL = closestDistRowL['Czolowy'].values
+frontWindH = closestDistRowH['Czolowy'].values
+backWindL = closestDistRowL['Tylni'].values
+backWindH = closestDistRowH['Tylni'].values
+tempDecL = closestDistRowL['TempSpad.'].values
+tempDecH = closestDistRowH['TempSpad.'].values
+tempIncL = closestDistRowL['TempWzros.'].values
+tempIncH = closestDistRowH['TempWzros.'].values
+airDensDecL = closestDistRowL['AirSpad.'].values
+airDensDecH = closestDistRowH['AirSpad.'].values
+airDensIncL = closestDistRowL['AirWzros.'].values
+airDensIncH = closestDistRowH['AirWzros.'].values
 
 #Higher table temp variables
-tempElevation=closestDistL-(((closestElevL-closestElevH)/(closestDistH-closestDistL))*(distance-closestDistL))
+tempElevation=closestElevL-(((closestElevL-closestElevH)/(closestDistH-closestDistL))*(distance-closestDistL))
 dElev100=closestDElev100L+(((closestDElev100H-closestDElev100L)/(closestDistH-closestDistL))*(distance-closestDistL))
 dFlightTime=dFlightTimeL+(((dFlightTimeH-dFlightTimeL)/(closestDistH-closestDistL))*(distance-closestDistL))
 tempFlightTime=flightTimeL-(((flightTimeL-flightTimeH)/(closestDistH-closestDistL))*(distance-closestDistL))
-windage=windageL(((windageL-windageH)/(closestDistH-closestDistL))*(distance-closestDistL))
+windage=float(windageL-(((windageL-windageH)/(closestDistH-closestDistL))*(distance-closestDistL)))
 frontWind=frontWindL+(((frontWindH-frontWindL)/(closestDistH-closestDistL))*(distance-closestDistL))
 backWind=backWindL+(((backWindH-backWindL)/(closestDistH-closestDistL))*(distance-closestDistL))
 tempDec=tempDecL+(((tempDecH-tempDecL)/(closestDistH-closestDistL))*(distance-closestDistL))
@@ -69,28 +65,39 @@ airDensInc=airDensIncL+(((airDensIncH-airDensIncL)/(closestDistH-closestDistL))*
 heightDiff=ownElevation-targetElevation
 heightDiffDeg=0
 if ownElevation>targetElevation:
-    heightDiffDeg=(int(heightDiff)/100)*dElev100
+    heightDiffDeg=(float(int(heightDiff))/100)*dElev100
 else:
-    heightDiffDeg=(int(heightDiff)/100)*dElev100*(-1)
+    heightDiffDeg=(float(int(heightDiff))/100)*dElev100*(-1)
+
 pressureDiff=pressure-1013.25
-pressureDiffDeg=0
+pressureDiffDeg=float(0)
 if pressureDiff<0:
     pressureDiffDeg=(pressureDiff/1013.25*100)*airDensDec*(-1)
 else:
     pressureDiffDeg=(pressureDiff/1013.25*100)*airDensInc*(-1)
-temperatureDiff=temperature-pressureDiff
-temperatureDiffDeg=0
+
+temperatureDiff=temperature-15
+temperatureDiffDeg=float(0)
 if temperatureDiff<0:
-    temperatureDiffDeg=int(temperatureDiff)*tempDec*(-1)
+    temperatureDiffDeg=float(int(temperatureDiff))*tempDec*(-1)
 else:
-    temperatureDiffDeg=int(temperatureDiff)*tempInc*(-1)
+    temperatureDiffDeg=float(int(temperatureDiff))*tempInc*(-1)
+
 windCorrectionDeg=0
 if wind<0:
-    windCorrectionDeg=int(wind)*backWind*(-1)
+    windCorrectionDeg=float(wind)*float(backWind)*(-1)
+    if windCorrectionDeg<0:
+        windCorrectionDeg*=-1
 else:
-    windCorrectionDeg=int(wind)*frontWind*(-1)
+    windCorrectionDeg=float(wind)*float(frontWind)*(-1)
+    if windCorrectionDeg<0:
+        windCorrectionDeg*=-1
 
 #Displayed info
 flightTime=tempFlightTime+(heightDiff/100*dFlightTime)
 finalElevation=tempElevation+heightDiffDeg+temperatureDiffDeg+pressureDiffDeg+windCorrectionDeg
-finalWindage=sideWind+windage
+finalWindage=sideWind*windage
+
+print("Flight time: "+str(flightTime)+"\n")
+print("Final elevation: "+str(finalElevation)+"\n")
+print("Final windage: "+str(finalWindage))
